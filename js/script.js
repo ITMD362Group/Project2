91b1b33446d00b13ff237e0021f01bb3a1bcd0bd
dgarcia123 <32852331+dgarcia123@users.noreply.github.com>
2020-04-24 20:03:41 -0500

    Adding some form validation for the payment page

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@ function AlertPage() {
   return false;
 }
 function submitPayment() {
-  alert("Thank you for paying!");
-  return false;
+  var fullname = document.forms["myForm"]["fullname"].value;
+  var cc = document.forms["myForm"]["cc"].value;
+  var date = document.forms["myForm"]["date"].value;
+  if ((fullname == "") || (cc == "") || (date == "")) {
+    alert("Remember to fill in all fields!");
+    return false;
+} else{
+    alert("Great, you're all signed up!")
+}
 }
